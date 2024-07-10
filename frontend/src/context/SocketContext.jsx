48055392e2d4cb51841e00c1270996479f321d9e
say-het <iam.lover.of.nature99@gmail.com>
2024-07-10 22:05:06 +0530

    I think its done

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider = ({children})=>{
     const {authUser} = useAuthContext();
     useEffect(()=>{
         if(authUser){
-            const socket = io("http://localhost:5000",{
+            const socket = io("https://real-time-chat-app-4oto.onrender.com/",{
                 query:{
                     userId:authUser._id,
                 },
